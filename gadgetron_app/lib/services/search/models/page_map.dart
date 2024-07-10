f338f37e08bd5795c2b7c1c3127b85e1b530ac3c
Scott Hatfield <contact@hatfieldscott.com>
2024-07-10 07:15:21 -0500

    Updated parsing of pagemap objects :framed_picture:

--- gadgetron_app/lib/services/search/models/page_map.dart
+++ gadgetron_app/lib/services/search/models/page_map.dart
@@ -1,3 +1,6 @@
+import 'package:gadgetron_app/services/search/models/cse_image.dart';
+import 'package:gadgetron_app/services/search/models/cse_thumbnail.dart';
+
 /// `PageMaps` is a structured data format that provides Google with information about the data on a page. It enables
 /// website creators to embed data and notes in webpages. Although this information is not visible to website
 /// visitors, Custom Search Engines recognize the data and use it to provide more relevant results. Because PageMaps
@@ -7,21 +10,29 @@
 /// See https://developers.google.com/custom-search/docs/structured_data#pagemaps for information about the responses
 /// received from CSE searches.
 class PageMap {
-  /// Information about the product.
-  //final Product? product;
+  /// A list of search result thumbnails for the search result.
+  final List<CseThumbnail>? cseThumbnails;
+
+  /// A list of search result images. These are generally larger in size than thumbnails.
+  final List<CseImage>? cseImages;
 
   // TODO(Toglefritz): parse other information like reviews
 
-  /// Creates a [PageMap] object with the specified product information.
-  PageMap(
-    //required this.product,
-  );
+  /// Creates a [PageMap] object with the specified information.
+  PageMap({
+    required this.cseThumbnails,
+    required this.cseImages,
+  });
 
   /// Returns a [PageMap] object parsed from the JSON returned by the Google Custom Search API.
   factory PageMap.fromJson(Map<String, dynamic> json) {
-
     return PageMap(
-      //product: null,
+      cseThumbnails: (json['cse_thumbnail'] as List<dynamic>?)
+          ?.map((dynamic thumbnail) => CseThumbnail.fromJson(thumbnail as Map<String, dynamic>))
+          .toList(),
+      cseImages: (json['cse_image'] as List<dynamic>?)
+          ?.map((dynamic image) => CseImage.fromJson(image as Map<String, dynamic>))
+          .toList(),
     );
   }
 }
