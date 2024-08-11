ada1accc2358525057f19ae932ea91ed4c7516ca
Scott Hatfield <contact@hatfieldscott.com>
2024-08-11 16:40:16 -0500

    Initialize Remote Configs :fire:

--- demo/presentation/lib/main.dart
+++ demo/presentation/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:launchpad_app/services/firebase_remote_config/remote_config_service.dart';
 import 'package:presentation/firebase_options.dart';
 import 'package:presentation/launchpad_presentation_app.dart';
 
@@ -24,6 +25,16 @@ Future<void> main() async {
     );
   } else {
     await FirebaseAppCheck.instance.activate();
+  }
+
+  // Initialize the Firebase Remote Config service.
+  try {
+    final RemoteConfigService remoteConfigService = RemoteConfigService();
+    await remoteConfigService.initialize();
+  } catch (e) {
+    debugPrint('Remote Config initialization failed with exception, $e');
+
+    // TODO(Toglefritz): Handle this exception
   }
 
   // Set the app to full-screen mode by hiding the status and navigation bars.
